57d54559b64dea3e1cefad7b98438120a53587b5
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-02-09 16:24:22 +0530

    [Jason] Added: UC4: Identify High-Performance Vessels

--- src/VesselUtil.java
+++ src/VesselUtil.java
@@ -17,5 +17,24 @@ public class VesselUtil {
         return null;
     }
 
+    public List<Vessel> getHighPerformanceVessels() {
+        List<Vessel> result = new ArrayList<>();
+
+        double maxSpeed = 0.0;
+        for (Vessel vessel : vesselList) {
+            if (vessel.getAverageSpeed() > maxSpeed) {
+                maxSpeed = vessel.getAverageSpeed();
+            }
+        }
+
+        for (Vessel vessel : vesselList) {
+            if (vessel.getAverageSpeed() == maxSpeed) {
+                result.add(vessel);
+            }
+        }
+
+        return result;
+    }
+
 }
 
